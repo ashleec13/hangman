a73b4eccb4aeaf247f2109e5fd748acdd3983015
ashleec13 <527achen@frhsd.com>
2026-03-09 11:48:23 -0400

    Enhance game layout and styling; add word banks for different difficulty levels

---
+++ script.js
@@ -0,0 +1,6 @@
+let wordBankEasy = ["snowman", "sleigh", "icicle", "frosty", "mittens", "hot chocolate", "frozen"];
+
+let wordBankMedium = ["candycane", "sledding", "winter hat", "hollyberry", "icehouse", "snowglobe", "snowangel"];
+
+let wordBankHard = ["blizzardly", "frostbite", "wintertime", "winter apparel", "winter wonderland", "ice palace", "polar expedition"];
+
